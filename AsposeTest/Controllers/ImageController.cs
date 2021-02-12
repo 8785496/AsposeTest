b601deec04ff91fac5dd81aa0499eab1f9d19039
Geman Chernyshov <h.chernyshov@velvetech.ru>
2021-02-13 04:10:23 +0700

    add image controller

---
+++ AsposeTest/Controllers/ImageController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsposeTest.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                var rootFolder = Directory.GetCurrentDirectory();
+                var filePath = Path.Combine(rootFolder, "Uploads", file.FileName);
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { name = file.FileName });
+        }
+
+        [HttpGet]
+        public IActionResult GetImage(string fileName)
+        {
+            var rootFolder = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(rootFolder, "Uploads", fileName);
+            var image = System.IO.File.OpenRead(filePath);
+            return File(image, "image/jpeg");
+        }
+    }
+}
--- AsposeTest/Startup.cs
+++ AsposeTest/Startup.cs
@@ -25,7 +25,7 @@ namespace AsposeTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,8 +35,14 @@ namespace AsposeTest
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -44,7 +50,9 @@ namespace AsposeTest
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
